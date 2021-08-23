412619ffaf036b292a3f3ea5601dafae3de6b80c
Narkomanser <ilyayatsukovich@gmail.com>
2021-08-23 21:01:51 +0300

    update paths for classes in build.cs file

--- Source/Iced/Iced.Build.cs
+++ Source/Iced/Iced.Build.cs
@@ -11,6 +11,12 @@ public class Iced : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PublicIncludePaths.AddRange(new string[]
+		{
+			"Iced/Public/Player",
+			"Iced/Public/GameModes"
+		});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
